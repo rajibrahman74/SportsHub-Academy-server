f50d3ecaa4a83258e53ad415526ab4e41ca77a09
rajibrahman74e <rajiburrahman74@gmail.com>
2023-06-11 05:12:00 +0600

    [ADD]-make-user-to-admin-process

--- index.js
+++ index.js
@@ -93,6 +93,35 @@ async function run() {
 
 
 
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if (req.decoded.email !== email) {
+          res.send({ admin: false })
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" }
+      res.send(result)
+  })
+
+
+  app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+          $set: {
+              role: "admin",
+          },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+  });
+
+
+
+
+
 
 
     // Send a ping to confirm a successful connection
