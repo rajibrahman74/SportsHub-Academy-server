3f9dc072e8c2aed46f73b599a22d2f8136c3e1f6
rajibrahman74e <rajiburrahman74@gmail.com>
2023-06-11 05:08:05 +0600

    [ADD]-verify-admin-function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
+var jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // midleware
@@ -48,14 +49,29 @@ async function run() {
     const allDataCollection = client.db("sportsDB").collection("all-data");
     const usersCollection = client.db("sportsDB").collection("users");
 
+
+
     // jwt token process
     app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
       res.send({ token });
     });
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+          return res.status(403).send({ error: true, message: "Forbidden Access" })
+      }
+      next()
+  }
+
+
+
 
     app.get("/alldata", async (req, res) => {
       const data = await allDataCollection.find().toArray();
@@ -63,16 +79,21 @@ async function run() {
     });
 
     // users collection
-    app.get("/users", verifyJWT, async (req, res) => {
+    app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
+
 
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
